ee3d4f42f079dcb77f6e0680acdb1d43e29d35e9
David Alves Dutra <dadutra@hotmail.com>
2025-02-01 08:02:36 -0300

    Base project.

---
+++ src/main/java/com/dutra/mailsender/MailsenderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dutra.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsenderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dutra/mailsender/MailsenderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailsenderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
